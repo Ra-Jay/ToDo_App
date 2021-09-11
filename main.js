9a5471d8d0d6d97e296272e2f9957a2605dbac7f
Ra-Jay <58256966+Ra-Jay@users.noreply.github.com>
2021-09-11 21:05:00 +0800

    update

--- main.js
+++ main.js
@@ -1,18 +1,6 @@
 var list = document.getElementById("categoryList");
 var input = document.getElementById("input");
 var button = document.getElementById("button");
-/*
-button.onclick = ()=>{ //when user click on plus icon button
-  let newTask = input.value; //getting input field value
-  let getLocalStorageData = localStorage.getItem("New Todo"); //getting localstorage
-  if(getLocalStorageData == null){ //if localstorage has no data
-    arrayList = []; //create a blank array
-  }else{
-    arrayList = JSON.parse(getLocalStorageData);  //transforming json string into a js object
-  }
-  arrayList.push(newTask); //pushing or adding new value in array
-  localStorage.setItem("New Todo", JSON.stringify(arrayList)); //transforming js object into a json string
-}*/
 
 button.addEventListener("click", function(){
 	var newTask = document.createElement("li");
