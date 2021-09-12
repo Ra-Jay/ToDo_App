8d156668591ee47c2a451f0fdee21a98b10be1f7
Ra-Jay <58256966+Ra-Jay@users.noreply.github.com>
2021-09-12 10:31:47 +0800

    update

--- main.js
+++ main.js
@@ -1,30 +1,50 @@
 var list = document.getElementById("categoryList");
 var input = document.getElementById("input");
 var button = document.getElementById("button");
+var listItem = document.getElementsByTagName("li");
 
+// Add a new task to the list when you click the "+" button
 button.addEventListener("click", function(){
 	var newTask = document.createElement("li");
-	newTask.innerHTML = input.value;
-	if(input.value=="")
+	newTask.innerHTML = `<li>${input.value}<span class="remove"><ion-icon class="trash" name="trash"></ion-icon></span><span class="edit"><ion-icon class="create" name="create"></ion-icon></span></li>`;
+	if(input.value=="") // Alerts when you add an empty value
 		alert("Enter a task");
 	else{
 		list.appendChild(newTask); 
 		input.value="";
 	}
+	// Marks the task done(line-through) when you click once
+	newTask.addEventListener("click", function(){
+		newTask.style.textDecoration= "line-through";
+		newTask.style.opacity = 0.5;
+	})
+	// Removes the task form the list when you click twice
+	newTask.addEventListener("dblclick", function(){
+		list.removeChild(newTask);
+	})
 })
-		
+	
+// Add a new task to the list when you press enter key
 document.addEventListener("keyup", function(event){
 	var newTask = document.createElement("li");
-	newTask.innerHTML = input.value;
+	newTask.innerHTML = `<li>${input.value}<span class="remove"><ion-icon class="trash" name="trash"></ion-icon></span><span class="edit"><ion-icon class="create" name="create"></ion-icon></span></li>`;
 	if(event.keyCode==13){
 		if(input.value=="")
 			alert("Enter a task");
 		else{
 			list.appendChild(newTask); 
 			input.value="";
+		}
 	}
-	}
+	newTask.addEventListener("click", function(){
+		newTask.style.textDecoration= "line-through";
+		newTask.style.opacity = 0.5;
+	})
+	newTask.addEventListener("dblclick", function(){
+		list.removeChild(newTask);
+	})
 })
+//updates the appearance of the "+" button when you hover above it 
 button.addEventListener("mouseover", function(){
 	button.style.backgroundColor= "#3baaff";
 });
@@ -34,5 +54,5 @@ button.addEventListener("mouseout", function(){
 });
 
 button.addEventListener("click", function(){
-	button.style.backgroundColor= "#0894ff";
+	button.style.backgroundColor= "#0994ff";
 });
