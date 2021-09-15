f82aac6c9ac758479ef5c1084040a875567f3a11
Ra-Jay <58256966+Ra-Jay@users.noreply.github.com>
2021-09-15 22:58:14 +0800

    update

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var list = document.getElementById("categoryList");
+var list = document.getElementById("taskList");
 var deleted = document.getElementById("deleted");
 var input = document.getElementById("input");
 var button = document.getElementById("button");
@@ -8,7 +8,7 @@ var trash = document.getElementsByTagName("span");
 // Add a new task to the list when you click the "+" button
 button.addEventListener("click", function(){
 	var newTask = document.createElement("li");
-	newTask.innerHTML = `<li>${input.value}<span onclick="remove()" class="remove"><ion-icon class="trash" name="trash"></ion-icon></span><span class="edit"><ion-icon class="create" name="create"></ion-icon></span></li>`;
+	newTask.innerHTML = `<p>${input.value}<span onclick="remove()" class="remove"><ion-icon class="trash" name="trash"></ion-icon></span><span class="edit"><ion-icon class="create" name="create"></ion-icon></span></p>`;
 	if(input.value=="") // Alerts when you add an empty value
 		alert("Enter a task");
 	else{
@@ -31,7 +31,7 @@ button.addEventListener("click", function(){
 // Add a new task to the list when you press enter key
 document.addEventListener("keyup", function(event){
 	var newTask = document.createElement("li");
-	newTask.innerHTML = `<li>${input.value}<span class="remove"><ion-icon class="trash" name="trash"></ion-icon></span><span class="edit"><ion-icon class="create" name="create"></ion-icon></span></li>`;
+	newTask.innerHTML = `<p>${input.value}<span onclick="remove()" class="remove"><ion-icon class="trash" name="trash"></ion-icon></span><span class="edit"><ion-icon class="create" name="create"></ion-icon></span></p>`;
 	if(event.keyCode==13){
 		if(input.value=="")
 			alert("Enter a task");
@@ -46,20 +46,8 @@ document.addEventListener("keyup", function(event){
 		newTask.style.opacity = 0.5;
 	})
 	// Removes the task form the list when you click twice and stores it in Deleted list
-	newTask.addEventListener("dblclick", function(){
+	/*newTask.addEventListener("dblclick", function(){
 		deleted.appendChild(newTask);
 		list.removeChild(newTask);
-	})
+	})*/
 })
-//updates the appearance of the "+" button when you hover above it 
-button.addEventListener("mouseover", function(){
-	button.style.backgroundColor= "#404040";
-});
-
-button.addEventListener("mouseout", function(){
-	button.style.backgroundColor= "#737373";
-});
-
-button.addEventListener("click", function(){
-	button.style.backgroundColor= "#262626";
-});
