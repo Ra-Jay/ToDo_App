d36280deb38b9c2738746daaff970c09709f167c
Ra-Jay <58256966+Ra-Jay@users.noreply.github.com>
2021-09-18 20:16:26 +0800

    update

--- main.js
+++ main.js
@@ -2,20 +2,27 @@ var list = document.getElementById("taskList");
 var deleted = document.getElementById("deleted");
 var input = document.getElementById("input");
 var button = document.getElementById("button");
-var deleteButton = document.getElementsByClassName("trash");
 
-
+//FUNCTIONS
 function addTask(){
+	//create a new div to store checkbox, li, edit button, and remove button
 	var newTask = document.createElement("div");
-	newTask.innerHTML =`<input class="checkStatus" type="Checkbox" name="" >
-						<li class="item" >${input.value}
-							<span class="edit">
-								<i class="fa fa-pencil create" aria-hidden="true"></i>
-							</span>
-							<span class="remove">
-								<i class="fa fa-trash trash" aria-hidden="true"></i>
-							</span>
-						</li>`;
+	newTask.innerHTML =`<input class="checkStatus" type="Checkbox" name="" >`;
+	//create new li to store the input value and append it to the newTask div
+	var item = document.createElement("li");
+	item.innerHTML = `${input.value}`;
+	newTask.appendChild(item);
+	//create span tag to store the edit icon and append it to the li tag
+	var editButton = document.createElement("span");
+	editButton.innerHTML = `<i class="fa fa-pencil create" aria-hidden="true"></i>`;
+	editButton.classList.add("edit");
+	item.appendChild(editButton);
+	//create span tag to store the remove icon and append it to the li tag
+	var trashButton = document.createElement("span");
+	trashButton.innerHTML = `<i class="fa fa-trash trash" aria-hidden="true"></i>`;
+	trashButton.classList.add("remove");
+	item.appendChild(trashButton);
+
 	if(input.value=="") // Alerts when you add an empty value
 		alert("Enter a task");
 	else{ // Adds the input value to the list
@@ -24,23 +31,30 @@ function addTask(){
 	}
 	// Marks the task done(line-through) when you click once
 	newTask.addEventListener("click", function(){
-		
+
 	})
 	// Removes the task form the list when you click twice and stores it in Deleted list
-	/*newTask.addEventListener("dblclick", function(){
+	newTask.addEventListener("dblclick", function(){
 		deleted.appendChild(newTask);
 		list.removeChild(newTask);
-	})*/
+	})
 }
 
-// Add a new task to the list when you click the "+" button
-button.addEventListener("click", function(){
-	//for(var i = 0; i<button.length;
-	addTask();
-})
+function deleteItem(e) { 
+  var item = e.target;
+  if(item.classList[0] === "remove"){
+	  var bin = item.parentElement;
+	  bin.parentElement.remove();
+  }
+}
 
-// Add a new task to the list when you press enter key
+//EVENT LISTENERS
+
+	// Add a new task to the list when you click the "+" button
+button.addEventListener("click", addTask);
+	// Add a new task to the list when you press enter key
 document.addEventListener("keyup", function(event){
 	if(event.keyCode==13)
 		addTask();
 })
+list.addEventListener("click", deleteItem);
